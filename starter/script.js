9f8708bcc80d1ed727c38151e25457252a727676
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-04 17:36:23 +0300

    Updated

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,18 @@
 let js = 'amazing';
-if (js === 'amazing') alert('javaScribt is FUN!');
-
+alert('JavaScript is for FUN!')
 40+8+23-10;
 console.log(40+8+23-10);
+// LECTURE: Values and Variables
+let firstName = 'Abdimalik';
+
+let myFristJob = "Coder";
+let myCurentJob = 'Programer';
+
+console.log(firstName);
+
+// Assigment of Values and Variables
+let country = 'Somalia';
+let continent = 'Africa';
+let population = '20m';
+
+// LECTURE: Data Types
