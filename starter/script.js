441afd0ab95243f2c7604f7695e907c331d4d6b9
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-04 22:25:26 +0300

    updated

--- starter/script.js
+++ starter/script.js
@@ -58,3 +58,32 @@ console.log(birthYear);
 // Assigment let, const and var
 language = 'Somali';
 console.log(language);
+
+// LECTURE 13 Basic operators
+
+// assigment operators 
+const now = 2023;
+const ageAbdimalik = now - 2004;
+const asiyaAge = now - 2005;
+console.log(ageAbdimalik, asiyaAge);
+
+console.log(ageAbdimalik * 2, asiyaAge / 2, 2 ** 3);
+
+const fristName = "Abdimalik";
+const lastName = "Abdirhaman";
+
+console.log(fristName + ' ' + lastName);
+// math operators
+let x = 10 + 5;
+x += 10; // x = x + 10 res 25
+x *= 4; // x = x * 4 res 100
+x++;
+x--;
+console.log(x);
+// comparison operator
+console.log(ageAbdimalik > asiyaAge);
+console.log(ageAbdimalik < 19);
+
+console.log(now - 2004 < now - 2005);
+
+// Lecture 14  Operator presedence
