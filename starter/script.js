d340d539415f5f3fc49f16315743543d5607103e
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-05 14:34:33 +0300

    updated

--- starter/script.js
+++ starter/script.js
@@ -101,6 +101,27 @@ console.log(myCountaryPopulation += 1000000);// Q2
 let finland = 6000000; // Q3
 console.log(myCountaryPopulation > finland); // Q3
 let description = 'Portugal is in Europe, and its 11 millionpeople speak portuguese';
+// CHALLENGE #1 
 
+const massMark = 78;
+const heightMark = 1.69; // meters
+const massJohn = 92;
+const heightJohn = 1.95;
+ 
+const BMIMark = massMark / (heightMark ** 2);
+const BMIJohn = massJohn / (heightJohn ** 2);
+ 
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn,markHigherBMI);
 
+// The challenge in my trying
+const culeskaAbdimalik = 66;
+const culeskaAsiya = 50;
+const dhirirkaAbdimalik = 1.74;
+const dhirirkaAsiya = 1.65;
+
+const BMIAbdimalik = culeskaAbdimalik / (dhirirkaAbdimalik ** 2);
+const BMIAsiya = culeskaAsiya / (dhirirkaAsiya ** 2);
+const abdimalikKaBadanAsiya = BMIAbdimalik > BMIAsiya;  
+console.log(BMIAbdimalik, BMIAsiya, abdimalikKaBadanAsiya);
 
