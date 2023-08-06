b51acc4b2cf56e7e64caec77c9bb46e85a988622
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-06 14:53:35 +0300

    updated

--- starter/challenges.js
+++ starter/challenges.js
@@ -1,3 +1,4 @@
+/*
 // CHALLENGE #1 
 const massMark = 78;
 const heightMark = 1.69; // meters
@@ -20,3 +21,25 @@ const BMIAbdimalik = culeskaAbdimalik / (dhirirkaAbdimalik ** 2);
 const BMIAsiya = culeskaAsiya / (dhirirkaAsiya ** 2);
 const abdimalikKaBadanAsiya = BMIAbdimalik > BMIAsiya;  
 console.log(BMIAbdimalik, BMIAsiya, abdimalikKaBadanAsiya);
+*/
+// CHALLENGE #2 
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+console.log("Mark's BMI is higher than John's!");
+
+console.log(`mark's BMI ${massMark} is higher than John's ${massJohn}`);
+
+// Another way
+if (BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!");
+} else {
+    console.log("Jhon's BMI is higher than Mark's");
+}
