3ef9d99e1fbac5a3292c50b73af1fc0a2266dfaf
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-06 14:53:07 +0300

    updated

--- starter/script.js
+++ starter/script.js
@@ -11,11 +11,6 @@ let myCurentJob = 'Programer';
 
 console.log(firstName);
 
-// Assigment of Values and Variables
-let country = 'Somalia';
-let continent = 'Africa';
-let population = '20m';
-
 // LECTURE: Data Types
 /* There are 7 Data Types 
 - Number : Floating point numbers and it's used for decimal and integers like let age = 19;
@@ -37,15 +32,6 @@ console.log(typeof 19);
 
 jS = 'Yes';
 
-// Assigment of Lecture 11 Data Types
-
-let isIsland = 140;
-let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-// console.log(typeof language);
-
 // LECTURE: let, const and var
 // this case called reasigning value of variable or mutation;
 let age = 18;
@@ -56,24 +42,8 @@ const birthYear = 2004;
 console.log(age);
 console.log(birthYear);
 
-// Assigment let, const and var
-language = 'Somali';
-console.log(language);
-
 // LECTURE 13 Basic operators
 
-// assigment operators 
-const now = 2023;
-const ageAbdimalik = now - 2004;
-const asiyaAge = now - 2005;
-console.log(ageAbdimalik, asiyaAge);
-
-console.log(ageAbdimalik * 2, asiyaAge / 2, 2 ** 3);
-
-const fristName = "Abdimalik";
-const lastName = "Abdirhaman";
-
-console.log(fristName + ' ' + lastName);
 // math operators
 let x = 10 + 5;
 x += 10; // x = x + 10 res 25
@@ -95,38 +65,40 @@ console.log(g);
 const avgAge = ageAbdimalik + asiyaAge / 2;
 console.log(ageAbdimalik, asiyaAge, avgAge);
 
-// Assigment For the 13 and 14 Lectures
-let myCountaryPopulation = 20000000; // Q1
-console.log(myCountaryPopulation /= 2);// Q1
-console.log(myCountaryPopulation += 1000000);// Q2
-let finland = 6000000; // Q3
-console.log(myCountaryPopulation > finland); // Q3
-let description = 'Portugal is in Europe, and its 11 millionpeople speak portuguese';
-// CHALLENGE #1 
-
-const massMark = 78;
-const heightMark = 1.69; // meters
-const massJohn = 92;
-const heightJohn = 1.95;
- 
-const BMIMark = massMark / (heightMark ** 2);
-const BMIJohn = massJohn / (heightJohn ** 2);
- 
-const markHigherBMI = BMIMark > BMIJohn;
-console.log(BMIMark, BMIJohn,markHigherBMI);
-
-// The challenge in my trying
-const culeskaAbdimalik = 66;
-const culeskaAsiya = 50;
-const dhirirkaAbdimalik = 1.74;
-const dhirirkaAsiya = 1.65;
-
-const BMIAbdimalik = culeskaAbdimalik / (dhirirkaAbdimalik ** 2);
-const BMIAsiya = culeskaAsiya / (dhirirkaAsiya ** 2);
-const abdimalikKaBadanAsiya = BMIAbdimalik > BMIAsiya;  
-console.log(BMIAbdimalik, BMIAsiya, abdimalikKaBadanAsiya);
-*/
 // LECTURE 17: Strings and Template Literals
 const fristName = 'Abdimalik';
 const job = 'Coder';
 const birthYear = 2004;
+const year = 2023;
+const abdimalik = "I am " + fristName + ', a ' + [year - birthYear] + ' year old ' + job + ' !.';
+
+console.log(abdimalik);
+
+const abdiNew = `I am ${fristName}, a ${year - birthYear} year old ${job} !.`;
+console.log(abdiNew);
+
+console.log('string with \n\
+multiple \n\
+lines \n\ ');
+console.log(`strings
+multiple 
+lines`);
+
+// LECTURE 18: Taking Decisions: if / else Statements
+const age = 13;
+
+if (age >= 18) {
+    console.log('Abdimalik can get driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Abdimalik is too young wait another ${yearsLeft} years `);
+}
+const birthYear = 2004;
+let centery;
+if(birthYear <= 2000) {
+     centery = 20;
+} else {
+     centery = 21;
+}
+console.log(centery);
+*/
