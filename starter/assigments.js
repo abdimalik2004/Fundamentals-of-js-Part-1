25644fc67b9164f4c427857f6ba3b4c3a8740f5f
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-06 13:21:02 +0300

    updated

---
+++ starter/assigments.js
@@ -0,0 +1,50 @@
+/*
+// Assigment of Values and Variables
+let country = 'Somalia';
+let continent = 'Africa';
+let population = '20m';
+
+// Assigment of Lecture 11 Data Types
+let isIsland = 140;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// Assigment let, const and var
+language = 'Somali';
+console.log(language);
+
+// assigment Lecture 12 : operators 
+const now = 2023;
+const ageAbdimalik = now - 2004;
+const asiyaAge = now - 2005;
+console.log(ageAbdimalik, asiyaAge);
+
+console.log(ageAbdimalik * 2, asiyaAge / 2, 2 ** 3);
+
+const fristName = "Abdimalik";
+const lastName = "Abdirhaman";
+
+console.log(fristName + ' ' + lastName);
+
+// Assigment For the 13 and 14 Lectures
+let myCountaryPopulation = 20000000; // Q1
+console.log(myCountaryPopulation /= 2);// Q1
+console.log(myCountaryPopulation += 1000000);// Q2
+let finland = 6000000; // Q3
+console.log(myCountaryPopulation > finland); // Q3
+let description = 'Portugal is in Europe, and its 11 millionpeople speak portuguese';
+
+// Lecture 15 and 16 are coding challenges
+*/
+// Assigment  LECTURE 17: Strings and Template Literals
+const description = 'Portugal is in Europe, and its 11 millionpeople speak portuguese';
+const country = 'Portugal';
+const continent = 'Europe';
+const number = 11;
+const population = ['millionpeople'];
+const language = 'portuguese';
+console.log(description);
+console.log(`${country} is in ${continent}, and its ${number} ${population} speak ${language}`);
