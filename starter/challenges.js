fe7f0813c8f5d4238d68433721d6b11fdc588468
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-11 18:05:58 +0300

    updated

--- starter/challenges.js
+++ starter/challenges.js
@@ -68,3 +68,7 @@ if (scoreDolphins > scoreKoalas) {
     console.log("Both win the trophy")
 }
 */
+// CHALLENEGE #4
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
