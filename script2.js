d8e9de9890df33e6200554f88d426a0fc9966ce8
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-12-02 17:13:01 +0300

    updated

---
+++ script2.js
@@ -0,0 +1,7 @@
+// let js = 'amaizing';
+// if (js === 'amaizing') alert('javascribt is FUN 😊');
+// let fristName = 'Abdimalik';
+// console.log(fristName);
+let country = 'somalia';
+let continent = 'Africa';
+let population = 20000000; //
